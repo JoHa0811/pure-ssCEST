d2928740a1a8638d3d5316a73cf743fca0f1518a
Johannes Hammacher <135006694+JoHa0811@users.noreply.github.com>
2025-05-26 12:45:14 +0200

    Add files via upload

--- reconstruction_examples.py
+++ reconstruction_examples.py
@@ -4,25 +4,44 @@ import matplotlib.pyplot as plt
 import colorcet as cc
 from visualize import plot_mtr_asym, calculate_mtr_asymmetry_df
 from reconstruct_data import reconstruct_folder, reconstruct_file
+import os
 
 #%%
 # reconstruct the data
 # access data from Zenodo
 
-record_id = '15489482'
+record_id = '15498440'
 api_url = f"https://zenodo.org/api/records/{record_id}"
 response = requests.get(api_url)
 data = response.json()
 
-# List all files
+#%%
+# Create a folder to store the downloaded files
+output_dir = f"zenodo_data"
+os.makedirs(output_dir, exist_ok=True)
+
+# Download all files
 for file in data['files']:
-    print(file['key'], ":", file['links']['self'])
+    filename = file['key']
+    url = file['links']['self']
+    if filename.endswith('with_traj.h5'):
+        print(f"Downloading {filename}...")
 
-# load data
+        response = requests.get(url)
+        file_path = os.path.join(output_dir, filename)
 
+        if os.path.exists(file_path):
+            print(f"Already exists: {file_path} — skipping download.")
+            continue
+        
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
 
-# reconstruct the data
+        print(f"Saved to {file_path}")
 
-# visualize the data
+#%%
+
+# reconstruct data
+reconstruct_folder(folderpath=output_dir)
 
 #%%
